ddc8c523998bf99f36cd74d4d5f2cbf5463f2f05
dwood96x <darksoldierx@gmail.com>
2019-01-28 14:01:21 -0500

    Added entity framework. Created database context class.

---
+++ MealTracker/Concrete/MTDBContext.cs
@@ -0,0 +1,21 @@
+﻿using MealTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MealTracker.Concrete
+{
+    public class MTDBContext : DbContext
+    {
+        public MTDBContext()
+            : base("name=MTDBContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
